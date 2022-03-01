553c1b88533b2c299c2abbf37c4bc96d3fb8b8d4
PsychedelicMonkey <ryan.getz.96@gmail.com>
2022-03-01 09:05:49 -0700

    Lightbox collection fix

--- resources/views/inc/gallery.blade.php
+++ resources/views/inc/gallery.blade.php
@@ -2,7 +2,7 @@
   <div class="gallery are-images-unloaded">
     <div class="gallery-sizer"></div>
     @foreach ($photos as $photo)
-      <a href="/storage/img/{{ $photo->img }}" class="gallery-item" data-sub-html=".caption" data-responsive="storage/img/sm-{{ $photo->img }} 450, storage/img/md-{{ $photo->img }} 850">
+      <a href="/storage/img/{{ $photo->img }}" class="gallery-item" data-sub-html=".caption" data-responsive="/storage/img/sm-{{ $photo->img }} 450, /storage/img/md-{{ $photo->img }} 850">
         <img src="/storage/img/gallery-{{ $photo->img }}" alt="">
 
         <div class="caption" style="display: none;">
