bb7a395e0748609caacdd05e3607168e2e50b67e
Ryan <45304409+PsychedelicMonkey@users.noreply.github.com>
2022-03-06 21:20:33 -0700

    About page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,5 +1,43 @@
 @extends('layouts.app')
 
 @section('content')
-  <h1>About</h1>
+  <section class="container">
+    <div class="container-header">
+      <img src="/storage/about/profile.jpg" alt="" class="profile-img">
+      <h1 class="title">I'm Jane Doe</h1>
+      <h4 class="subtitle">I'm a freelance photographer</h4>
+
+      <section class="about-content">
+        <div class="card">
+          <div class="text">
+            <h1>Lorem, ipsum dolor.</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, non architecto. Magni quidem, cupiditate saepe incidunt fugiat rem quia consectetur?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad vitae nostrum doloremque ipsum, consequatur porro labore optio atque quidem sed unde! Consequuntur animi dignissimos inventore architecto? Assumenda officiis omnis porro laudantium, sequi quisquam amet! Nihil quo obcaecati ipsum quidem veniam?</p>
+          </div>
+          <img src="/storage/about/img-1.jpg" alt="">
+        </div>
+        <div class="card">
+          <div class="text">
+            <h1>Lorem, ipsum dolor.</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, non architecto. Magni quidem, cupiditate saepe incidunt fugiat rem quia consectetur?</p>
+          </div>
+          <img src="/storage/about/img-2.jpg" alt="">
+        </div>
+        <div class="card">
+          <div class="text">
+            <h1>Lorem, ipsum dolor.</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, non architecto. Magni quidem, cupiditate saepe incidunt fugiat rem quia consectetur?</p>
+          </div>
+          <img src="/storage/about/img-3.jpg" alt="">
+        </div>
+        <div class="card">
+          <div class="text">
+            <h1>Lorem, ipsum dolor.</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, non architecto. Magni quidem, cupiditate saepe incidunt fugiat rem quia consectetur?</p>
+          </div>
+          <img src="/storage/about/profile.jpg" alt="">
+        </div>
+      </section>
+    </div>
+  </section>
 @endsection
